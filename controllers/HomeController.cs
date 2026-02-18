eb514698a5d2b5ca57199e552c6a75065001a894
wreckingralph06 <ralphwaldocandaza@gmail.com>
2026-02-18 15:01:00 +0800

    Added GET /brew-coffee API endpoint for Status Response Demo

---
+++ controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticAssets;
+using System;
+
+namespace coffeeTime.controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoffeeController : ControllerBase
+    {
+        public static int callCounter = 0;
+        public static string orderMessage = "Your piping hot coffee is ready";
+
+        [HttpGet("/brew-coffee")]
+        public IActionResult DisplayOrders()
+        {
+            callCounter++;
+
+            var todayDate = DateTime.Now;
+
+            //var todayDate = new DateTime(2026, 4, 1);
+            if (todayDate.Month == 4 && todayDate.Day == 1)
+            {
+                Response.StatusCode = 418;
+                return new EmptyResult();
+            }
+
+            if (callCounter % 5 == 0)
+            {
+                Response.StatusCode = 503;
+                return new EmptyResult();
+            }
+
+            var response = new
+            {
+                message = orderMessage,
+                prepared = DateTimeOffset.Now.ToString("o")
+            };
+
+            return Ok(response); 
+        }
+    }
+}
---
+++ tests/Class.cs
@@ -0,0 +1,59 @@
+﻿using coffeeTime.controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace coffeeTime.tests
+{
+    public class CoffeeControllerTests
+    {
+        private CoffeeController CreateController()
+        {
+            var controller = new CoffeeController();
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            return controller;
+        }
+
+        [Fact]
+        public void DisplayOrders_Return200Status()
+        {
+            var controller = CreateController();
+            var result = controller.DisplayOrders();
+
+            var response = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, response.StatusCode ?? 200);
+        }
+
+        [Fact]
+        public void DisplayOrders_Return503Status()
+        {
+            CoffeeController.callCounter = 0;
+
+            var controller = CreateController();
+            IActionResult result = null;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                result = controller.DisplayOrders();
+            }
+
+            Assert.IsType<EmptyResult>(result);
+            Assert.Equal(503, controller.Response.StatusCode);
+        }
+
+        [Fact]
+        public void DisplayOrders_Return418Status()
+        {
+            var controller = CreateController();
+            var result = controller.DisplayOrders();
+
+            Assert.IsType<EmptyResult>(result);
+            Assert.Equal(418, controller.Response.StatusCode);
+        }
+    }
+}
